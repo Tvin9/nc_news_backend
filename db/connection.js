c4a26144a51ae609fe259a93678228b068a41f59
Tvin9 <christopherharrison0606@gmail.com>
2026-02-10 17:15:30 +0000

    small fix to connection logic

--- db/connection.js
+++ db/connection.js
@@ -3,13 +3,12 @@ const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
 const config = {};
+require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` });
 
 if (ENV === 'production') {
 	config.connectionString = process.env.DATABASE_URL;
 	config.max = 2;
 }
-
-require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` });
 
 const db = new Pool(config);
 
